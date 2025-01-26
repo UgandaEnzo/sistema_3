7648b6d38f807eb4cc7a824ad4fa059e9d7594b0
Nigher Sifuentes <76107926+UgandaEnzo@users.noreply.github.com>
2025-01-26 15:55:23 -0400

    p1.2

--- dell/asgi.py
+++ dell/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sistema_3 project.
+ASGI config for dell project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistema_3.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dell.settings')
 
 application = get_asgi_application()
